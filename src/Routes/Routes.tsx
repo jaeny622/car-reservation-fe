b281876420771e099ad79ed3fbace58456c2b69c
jisu_oh <jisu_oh@tmax.co.kr>
2024-12-19 00:13:50 +0900

    [chore] project 초기 세팅

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -3,9 +3,8 @@ import { Routes, Route } from "react-router";
 
 import GlobalLayout from "../Layouts/GlobalLayout";
 
-const Trending = lazy(() => import("../Pages/Trending"));
+const Home = lazy(() => import("../Pages/Home"));
 const Join = lazy(() => import("../Pages/Join"));
-const Search = lazy(() => import("../Pages/Search"));
 const Login = lazy(() => import("../Pages/Login"));
 const Detail = lazy(() => import("../Pages/Detail"));
 const Edit = lazy(() => import("../Pages/Edit"));
@@ -16,10 +15,9 @@ export default function GlobalRoutes() {
   return (
     <Routes>
       <Route path="/" element={<GlobalLayout />}>
-        <Route index element={<Trending />} />
+        <Route index element={<Home />} />
         <Route path="join" element={<Join />} />
         <Route path="login" element={<Login />} />
-        <Route path="search" element={<Search />} />
         <Route path="video">
           <Route path="upload" element={<Upload />} />
           <Route path="edit" element={<Edit />} />
