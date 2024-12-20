84e3295a141433609ab96aa1ec1c6ab735d11cb6
jisu_oh <jisu_oh@tmax.co.kr>
2024-12-20 16:15:14 +0900

    [style] ReservCalendar 스타일 수정 - max-width 1300px로 설정 및 가운데 위치하도록 적용

--- src/Components/ReservCalendar/ReservCalendar.styles.tsx
+++ src/Components/ReservCalendar/ReservCalendar.styles.tsx
@@ -3,4 +3,9 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 100%;
   height: 100%;
+
+  display: flex;
+  justify-self: center;
+
+  max-width: 1300px;
 `;
